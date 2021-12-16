de0fb445409593b48c2b297ec3b3b644b08501e5
Alexandre MARIE <Alex.m117@outlook.fr>
2021-12-16 17:55:26 +0100

    mise à jour P6 like et delete

--- README.md
+++ README.md
@@ -1,19 +1,45 @@
 # Projet 6 d'Openclassrooms: Piquante #
 
 Réalisation du projet par **Alexandre MARIE**
+## Piquante: Construisez une API sécurisée pour une application d'avis gastronomiques. ##
 
-## Validation
+- Implémenter un modèle logique de données conformément à la réglementation.
+- Stocker des données de manière sécurisée.
+- Mettre en œuvre des opérations CRUD de manière sécurisée.
+- Mettre en place une API REST.
 
-- Validation HTML5: https://validator.w3.org/
-- Validation CSS3: https://validator.w3.org/unicorn/
+### Conditions préalables à l'initialisation du front-end ###
 
+- Vous aurez besoin d'avoir Node et `npm` installés localement sur votre machine.
+- Les dépendances suivantes doivent êtres installées pour lancer l'application:
+    - NodeJS 12.14 or 14.0.+
+    - Angular CLI 7.0.2.+
+    - Node-sass
 
-## Installation front-end ##
+A la racine du dossier front-end via le terminal saisissez `run npm install` si windows ou `sudo npm install` pour macOs (sudo: mot de passe necessaire) et ensuite `npm install --save-dev run-script-os` 
 
-Exécutez « npm start ». Cela devrait à la fois exécuter le serveur local et lancer votre navigateur.
+## Initalisation front-end ##
 
+Il suffit ensuite d'utiliser le terminal via la commande `cd` ce rendre à la racine du dossier `front-end`. Une fois positionné sur le dossier `front-end` exécutez « npm start ». Cela devrait à la fois exécuter le serveur local et lancer votre navigateur.
 Si votre navigateur ne démarre pas ou affiche une erreur 404, accédez à http://localhost:8080 dans votre navigateur.
-
 L'application devrait se recharger automatiquement lorsque vous modifiez un fichier.
-
 Utilisez `Ctrl+C` dans le terminal pour arrêter le serveur local.
+  
+### Conditions préalables à l'initialisation du back-end ###
+Vous aurez besoin d'avoir Node et `npm` installés localement sur votre machine.
+A la racine du dossier back-end via le terminal saisissez `run npm install` si windows ou `sudo npm install` pour macOs (sudo: mot de passe necessaire) 
+
+- Créer un dossier "images" (ou seront stockés les images des sauces)
+
+- Renommer le fichier env.exemple fourni dans le dossier back-end en ".env" pour intégrer le pattern si dessous et y mettre vos informations personnelles (base de données, clé secrète ect...).
+
+PORT = 3000
+DB_USERNAME = "Username de la base de donnée MONGO_DB"
+DB_PASSWORD = "Password de la base de donnée MONGO_DB"
+DB_CLUSTER = "Cluster de votre base de donnée au format "(Clusterx.xxxx)"
+DB_NAME = "Mon de votre base de donnée MONGO_DB"
+CRYPTOJS_EMAIL = "Votre clé crypto secrète (Chaine de caractère à insérer via générateur de mot de passe recommandé)"
+JWT_KEY_TOKEN = "Votre token secret (Chaine de caractère à insérer via générateur de mot de passe recommandé)"
+## Initialisation back-end ##
+
+Il suffit ensuite d'utiliser le terminal via la commande `cd` ce rendre à la racine du dossier `back-end`. Une fois positionné sur le dossier `back-end` saisissez sur le terminal `node server.js`, il devrait alors y avoir un message de confirmation "Listening on port 3000". 
--- back-end/controllers/sauce.js
+++ back-end/controllers/sauce.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 // Création de la sauce
 exports.createSauce = (req, res, next) => {
  
-
   console.log(`----->CONTENU : req.body - controllers/sauce`);
   const obj = JSON.parse(JSON.stringify(req.body));
 
@@ -39,7 +38,7 @@ exports.createSauce = (req, res, next) => {
   .catch((error) => { res.status(400).json({ error });
   })  
 };
-
+// Récupére via GET la sauce via sont ID en format JSON.
 exports.getOneSauce = (req, res, next) => {
   Sauce
   .findOne({ _id : req.params.id })
@@ -48,7 +47,7 @@ exports.getOneSauce = (req, res, next) => {
   .catch((error) => { res.status(404).json({ error });
   });
 };
-
+// Récupére via GET toutes les sauces en JSON.
 exports.getAllSauce = (req, res, next) => {
   Sauce
   .find().then((sauces) => { res.status(200).json(sauces);
@@ -56,7 +55,7 @@ exports.getAllSauce = (req, res, next) => {
   .catch((error) => { res.status(400).json({ error });
   });
 };
-
+// Modifie via la méthode PUT la sauce via sont ID (champs et image via multer)
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ? {
     ...JSON.parse(req.body.sauce),
@@ -70,7 +69,7 @@ exports.modifySauce = (req, res, next) => {
     .catch((error) => { res.status(400).json({ error: error });
     })
 };
-
+// Suppression de la sauce avec contrôle du userId pour empêcher la suppression d'une sauce d'un autre utilisateur.
 exports.deleteSauce = (req, res, next) => {   
   Sauce
     .findOne({ _id : req.params.id })
@@ -82,6 +81,7 @@ exports.deleteSauce = (req, res, next) => {
       if (!sauce) {
         return res.status(401).json ({ error: 'Sauce non trouvée !' });
         }
+        // Si le userId lié à la sauce et la même que dans celui decoder dans le token ont supprime l'mage du dossier "images" ainsi que la sauce.
       if (sauce.userId === req.token.userId) { 
         const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => { 
@@ -103,7 +103,7 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 exports.likeSauce = (req, res, next) => { 
-// Déclaration des constantes pour implémenter le système de vote "$pull / $inc / $push" de MongoDB"
+// Déclaration des constantes pour implémenter le système de vote "$pull / $inc / $push" de MongoDB".
 const sauceId = req.params.id;
 const userId = req.body.userId;
 const userLike = req.body.like;
@@ -113,37 +113,43 @@ const userLike = req.body.like;
     Sauce
       .findOne({ _id : sauceId })
       .then((sauce) => {
-      // Import des tableaux du schema sauce pour requeillir le nombre de Likes total
-      const usersLiked = sauce.userLiked;
+      // Import des tableaux du schema sauce pour requeillir le nombre de Likes total.
+      const usersLiked = sauce.usersLiked;
       const usersDisliked = sauce.usersDisliked;
       console.log(usersLiked)
       console.log(usersDisliked)
       
       // Contrôle en effectuant une recherche de l'userId si l'user à déjà effectué un vote.
-      const userLiked = usersLiked.find((id) => id === userId);
+      const userLiked = userLiked.find((id) => id === userId);
       const userDisliked = userDisliked.find((id) => id === userId);
       console.log(userLiked)
       console.log(userDisliked)
+
     // Si l'utilisateur à déjà "like" la sauce, le like est retiré ainsi que sont id dans le tableau "usersLiked".
     if( userLiked ) {
       Sauce
       .updateOne({ _id : sauceId },
       {
+      // Incrémente la valeur pour -1 pour "likes".
        $inc: { likes: -1 },
+      // Supprime le userId du tableau "usersLiked".
        $pull: { usersLiked: userId }
       }
       )
-      .then(() => { res.status(201).json({ message: "Vote enregisté !" });
+      .then(() => { res.status(201).json({ message: "Like retiré !" });
       })
       .catch(error => { res.status(400).json({ error });
       });
     }
-    // Si l'utilisateur à déjà "dislike" la sauce, le dislike est retiré ainsi que sont id dans le tableau "usersDisliked"
+
+    // Si l'utilisateur à déjà "dislike" la sauce, le dislike est retiré ainsi que sont id dans le tableau "usersDisliked".
     if( userDisliked ) {
       Sauce
       .updateOne({ _id : sauceId },
       {
+        // Incrémente la valeur pour -1 pour "dislikes".
         $inc: { dislikes: -1 },
+        // Supprime le userId du tableau "usersDisliked".
         $pull: { usersDisliked: userId }
       }
       )
@@ -156,12 +162,15 @@ const userLike = req.body.like;
   .catch((error) => {res.status(404).json({ error });
   });  
   };
+
   // Si l'utilisateur n'à pas encore "like" la sauce, le like est ajouter ainsi que sont id dans le tableau "usersLiked".
   if ( userLike === 1 ) {
     Sauce
       .updateOne({ _id : sauceId },
       {
+        // Incrémente la valeur pour 1 pour "likes".
         $inc: { likes: 1 },
+        // Ajoute le userId dans le tableau "usersLiked".
         $push: { usersLiked: userId }
       })
       .then(() => { res.status(201).json({ message: "Vote enregisté !" });
@@ -170,12 +179,15 @@ const userLike = req.body.like;
       .catch(error => { res.status(400).json({ error });
       });
   };
-  // Si l'utilisateur n'à pas encore "dislike" la sauce, le dislike est ajouter ainsi que sont id dans le tableau "usersDisliked"
+
+  // Si l'utilisateur n'à pas encore "dislike" la sauce, le dislike est ajouter ainsi que sont id dans le tableau "usersDisliked".
   if ( userLike === -1 ) {
     Sauce
       .updateOne({ _id : sauceId },
       {
+        // Incrémente la valeur pour 1 pour "dislikes".
         $inc: { dislikes: 1 },
+        // Ajoute le userId dans le tableau "usersDisliked".
         $push: { usersDisliked: userId }
       }
       )
